855d18a7c4b0ea6c8b38a14984b51bba2fc43211
Sarah Goossens <s.goossens@noviuniversity.com>
2020-04-27 15:17:00 +0200

    first commit

---
+++ src/main/java/com/xsarahg/CurrencyApp/CurrencyAppApplication.java
@@ -0,0 +1,13 @@
+package com.xsarahg.CurrencyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xsarahg/CurrencyApp/CurrencyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsarahg.CurrencyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
